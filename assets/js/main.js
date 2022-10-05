dda03d3428b70bc3a666ee88b9aeca33b54129d0
sgraziola <simo.fdl@gmail.com>
2022-10-05 15:20:54 +0200

    Ex Unsecured Pwd: final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,11 @@
 //poi chiedi il suo cognome,
 //poi chiedi il suo colore preferito
 //Infine scrivi sulla pagina nomecognomecolorepreferito22
+
+const userName = prompt("Come ti chiami?")
+//console.log(userName);
+const userSurname = prompt("Qual'è il tuo cognome?")
+//console.log(userSurname);
+const userColor = prompt("Qual'è il tuo colore preferito?")
+const currentYear = 2022
+document.getElementById("unsecured_pwd").innerHTML = (`La tua password supersegretissima è ${userName}${userSurname}${userColor}${currentYear}`)
